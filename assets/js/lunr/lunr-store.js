33d78c606aaaf827b553ab53636b3ab2c4df601a
KZRT <KZRT@users.noreply.github.com>
2023-05-18 11:43:51 +0000

    jekyll build from Action 2e51a90225d236fd75c680502a7c5ee26da31fcf

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Adding Latex Support to Github Pages that are using Jekyll",
+        "excerpt":"$[ x^n + y^n = z^n ]$  ","categories": [],
+        "tags": ["LaTeX","Github Pages","Jekyll","MathJax"],
+        "url": "/Adding-latex-support/",
+        "teaser": null
+      },{
+        "title": "Jekyll과 Jekyll Theme으로 Github Page 만들기",
+        "excerpt":"https://github.com/inukshuk/jekyll-scholar/issues/203 Github Pages a[1] 1. [1]D. Flanagan and Y. Matsumoto, The Ruby Programming Language. O’Reilly Media, 2008. &lt;!-- Bibliography --&gt; &lt;bibliography entry-spacing=\"0\" second-field-align=\"flush\"&gt; &lt;layout&gt; &lt;!-- Citation Number --&gt; &lt;text variable=\"citation-number\" prefix=\"[\" suffix=\"] \"/&gt; 1. [1] D. Flanagan and Y. Matsumoto, The Ruby Programming Language. O’Reilly Media, 2008. • [1] D....","categories": [],
+        "tags": ["Github Pages","Jekyll","Minimal-Mistakes"],
+        "url": "/Github-pages-with-Jekyll/",
+        "teaser": null
+      }]
